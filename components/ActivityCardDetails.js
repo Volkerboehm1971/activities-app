ae6c855d4f694e6d9f971ea0ca220ab5a4808b07
Richard Beindorf <richard_beindorf@web.de>
2024-04-26 16:38:36 +0200

    image is being display on details page

--- components/ActivityCardDetails.js
+++ components/ActivityCardDetails.js
@@ -1,13 +1,30 @@
 import styled from "styled-components";
 import Image from "next/image";
 import { activities } from "@/lib/dummydata";
+import { useRouter } from "next/router";
 
 
 export default function ActivityCardDetails(){
-    return (
-        <div>
-            Hello World
-        </div>
+    const router = useRouter();
+    const { id } = router.query;
 
+
+    const detailActivity = activities.find((activity) => activity.id === id);
+
+    if (!detailActivity) {
+        return null;
+      }
+
+ 
+
+    const { image } = detailActivity;
+    console.log(image);
+
+    console.log(detailActivity);
+
+    return (
+        <>
+            <Image src={image} width={187.5} height={140.5} alt="hiker on a mountain"/>
+        </>
     );
-}
+} 
