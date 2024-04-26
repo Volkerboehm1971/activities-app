a4950b78076fe7c1234ced247ae721034fc48570
Richard Beindorf <richard_beindorf@web.de>
2024-04-26 17:05:33 +0200

    fine tuned how we catch the right activity based on id - edge cases

--- components/ActivityCardDetails.js
+++ components/ActivityCardDetails.js
@@ -8,23 +8,15 @@ export default function ActivityCardDetails(){
     const router = useRouter();
     const { id } = router.query;
 
-
     const detailActivity = activities.find((activity) => activity.id === id);
 
     if (!detailActivity) {
         return null;
       }
 
- 
-
-    const { image } = detailActivity;
-    console.log(image);
-
-    console.log(detailActivity);
-
     return (
         <>
-            <Image src={image} width={187.5} height={140.5} alt="hiker on a mountain"/>
+            <Image src={detailActivity.image} width={187.5} height={140.5} alt="hiker on a mountain"/>
         </>
     );
 } 
