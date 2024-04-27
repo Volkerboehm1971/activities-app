082e4a3572a77e4622b86ca068f2d4899fdac2af
Richard Beindorf <richard_beindorf@web.de>
2024-04-27 21:15:54 +0200

    basic styling for details page done, adding back button

--- components/ActivityCardDetails.js
+++ components/ActivityCardDetails.js
@@ -3,20 +3,68 @@ import Image from "next/image";
 import { activities } from "@/lib/dummydata";
 import { useRouter } from "next/router";
 
+const StyledButton = styled.button`
+    position: relative;
+    
+`
+const StyledSection = styled.section`
+    margin-left: 8vw;
+    margin-right: 5vw;
+    margin-top: 10vw;
+`
+
+const StyledImage = styled(Image)`
+   position: relative;
+
+`
+
+const StyledSubheader = styled.h5`
+    margin-bottom: 0;
+`
+
+const Styledinfo = styled.p`
+    margin-top: 1vw;
+`
+
+const Styledarticle = styled.p`
+    line-height: 1.4;
+    font-size: small;
+    margin-top: 2vw;
+    text-align: justify;
+    padding-right: 5vw;
+`
+
 
 export default function ActivityCardDetails(){
     const router = useRouter();
     const { id } = router.query;
 
     const detailActivity = activities.find((activity) => activity.id === id);
+    console.log(detailActivity);
 
     if (!detailActivity) {
         return null;
       }
 
     return (
-        <>
-            <Image src={detailActivity.image} width={187.5} height={140.5} alt="hiker on a mountain"/>
+        <>  
+            <StyledButton></StyledButton>
+            <StyledImage src={detailActivity.image} width={187.5} height={140.5} alt="hiker on a mountain"/>
+            <StyledSection>
+
+                <StyledSubheader>Type of Tour</StyledSubheader>
+                    <Styledinfo>{detailActivity.title}</Styledinfo>
+
+                <StyledSubheader>Area</StyledSubheader>
+                    <Styledinfo> {detailActivity.area}</Styledinfo>
+
+                <StyledSubheader>Country</StyledSubheader>
+                    <Styledinfo> {detailActivity.country}</Styledinfo>
+
+                <StyledSubheader>Description</StyledSubheader>
+                    <Styledarticle>{detailActivity.description}</Styledarticle> 
+
+            </StyledSection>
         </>
     );
 } 
