e62b2e596540f90312c0154be20a7f5ac76db4c6
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-05-07 11:40:56 +0200

    Adding SearchBar to the top of the activities list, implementing functionality

--- pages/activityList.js
+++ pages/activityList.js
@@ -1,6 +1,7 @@
 import ActivityCard from "@/components/ActivityCard";
 import styled from "styled-components";
 import Link from "next/link";
+import { useState } from "react";
 
 const StyledHeadline = styled.h1`
   text-align: center;
@@ -35,12 +36,72 @@ const StyledLi = styled.li`
   padding: 20px 0 20px 0;
 `;
 
+const StyledSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+`;
+
+const WrapperSearchBar = styled.div`
+  position: relative;
+  width: 80%;
+`;
+
+const InputSearchField = styled.input`
+  padding: 0.5rem 0.5rem 0.5rem 30px;
+  border: 2px solid black;
+  border-radius: 0.5rem;
+  width: 100%;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M23.707,22.293l-5.969-5.969a10.016,10.016,0,1,0-1.414,1.414l5.969,5.969a1,1,0,0,0,1.414-1.414ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z' fill='black'/%3E%3C/svg%3E");
+  background-repeat: no-repeat;
+  background-position: 5px center;
+  background-size: 20px;
+  margin-bottom: 20px;
+`;
+
 export default function ActivityList({ activities }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleOnChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const getFilteredActivities = (searchTerm, activities) => {
+    if (!searchTerm) {
+      return activities;
+    }
+    return activities.filter(
+      (activity) =>
+        activity.category.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        activity.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        activity.area.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        activity.country.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  };
+
+  const filteredActivities = getFilteredActivities(searchTerm, activities);
+
+  console.log(searchTerm);
+  console.log(filteredActivities);
+
   return (
     <>
       <StyledHeadline>List of Activities</StyledHeadline>
+      <StyledSection>
+        <label htmlFor="image">Search Activities</label>
+        <WrapperSearchBar>
+          <InputSearchField
+            id="image"
+            name="image"
+            type="search"
+            value={searchTerm}
+            onChange={handleOnChange}
+            required
+          />
+        </WrapperSearchBar>
+      </StyledSection>
       <StyledUl>
-        {activities.map((activity) => (
+        {filteredActivities.map((activity) => (
           <>
             <StyledLinkDetailsPage href={`/${activity.id}`}>
               <StyledLi key={activity.id}>
