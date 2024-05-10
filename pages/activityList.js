aace9cee98c2f46bef828fb1587ba231f9ea91cb
Richard Beindorf <richard_beindorf@web.de>
2024-05-10 17:11:05 +0200

    weird navbar bug in vercel

--- pages/activityList.js
+++ pages/activityList.js
@@ -1,81 +1,7 @@
 import ActivityCard from "@/components/ActivityCard";
-import styled from "styled-components";
-import Link from "next/link";
 import { useState } from "react";
 import CatetoryFilters from "@/components/CatetoryFilters";
-
-const StyledHeadline = styled.h1`
-  text-align: center;
-`;
-
-const StyledUl = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-  padding: 0 0 85px 0;
-  margin: 0 2rem 0 2rem;
-`;
-
-const StyledLinkDetailsPage = styled(Link)`
-  color: black;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-decoration: none;
-
-  &:active {
-    color: black;
-  }
-`;
-
-const StyledLi = styled.li`
-  border-style: solid;
-  border-color: black;
-  border-width: 1px;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
-  padding: 20px 0 20px 0;
-  list-style-type: none;
-`;
-
-const StyledSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  gap: 0.6rem;
-`;
-
-const LabelSearchField = styled.label`
-  display: block;
-  text-align: center;
-  font-size: medium;
-  font-weight: bold;
-`;
-
-const WrapperSearchBar = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const InputSearchField = styled.input`
-  padding: 8px 8px 8px 40px;
-  border: 2px solid black;
-  border-radius: 0.5rem;
-  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath d='M23.707,22.293l-5.969-5.969a10.016,10.016,0,1,0-1.414,1.414l5.969,5.969a1,1,0,0,0,1.414-1.414ZM10,18a8,8,0,1,1,8-8A8.009,8.009,0,0,1,10,18Z' fill='black'/%3E%3C/svg%3E");
-  background-repeat: no-repeat;
-  background-position: 10px center;
-  background-size: 20px;
-  width: 310px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
-`;
-
-const ErrorMessage = styled.p`
-  color: red;
-  font-size: medium;
-  font-weight: bold;
-  text-align: center;
-  margin: 0 0 25px 0;
-`;
+import { Headline, Ul, LinkDetailsPage, Li, Section, LabelSearchField, WrapperSearchBar, InputSearchField, ErrorMessage } from "../components/styledComponents/activityList.styles";
 
 export default function ActivityList({ activities }) {
   const [searchTerm, setSearchTerm] = useState("");
@@ -115,8 +41,8 @@ export default function ActivityList({ activities }) {
 
   return (
     <>
-      <StyledHeadline>List of Activities</StyledHeadline>
-      <StyledSection>
+      <Headline>List of Activities</Headline>
+      <Section>
         <LabelSearchField htmlFor="image">Filter Activities</LabelSearchField>
         <WrapperSearchBar>
           <InputSearchField
@@ -135,43 +61,43 @@ export default function ActivityList({ activities }) {
           handleIconClick={handleIconClick}
           selectedByIcon={selectedByIcon}
         />
-      </StyledSection>
+      </Section>
 
       {filteredActivities.length > 0 ? (
-        <StyledUl>
+        <Ul>
           {filteredActivities.map((activity) => (
-            <StyledLi key={activity.id}>
-              <StyledLinkDetailsPage href={`/${activity.id}`}>
+            <Li key={activity.id}>
+              <LinkDetailsPage href={`/${activity.id}`}>
                 <ActivityCard
                   id={activity.id}
                   image={activity.image}
                   title={activity.title}
                   area={activity.area}
                 />
-              </StyledLinkDetailsPage>
-            </StyledLi>
+              </LinkDetailsPage>
+            </Li>
           ))}
-        </StyledUl>
+        </Ul>
       ) : (
         <div>
           <ErrorMessage>
             There is no matching result for your search! <br /> Please try
             again!
           </ErrorMessage>
-          <StyledUl>
+          <Ul>
             {activities.map((activity) => (
-              <StyledLi key={activity.id}>
-                <StyledLinkDetailsPage href={`/${activity.id}`}>
+              <Li key={activity.id}>
+                <LinkDetailsPage href={`/${activity.id}`}>
                   <ActivityCard
                     id={activity.id}
                     image={activity.image}
                     title={activity.title}
                     area={activity.area}
                   />
-                </StyledLinkDetailsPage>
-              </StyledLi>
+                </LinkDetailsPage>
+              </Li>
             ))}
-          </StyledUl>
+          </Ul>
         </div>
       )}
     </>
