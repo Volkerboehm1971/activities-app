26146abb789caadbefe108e5a2c81d3fad567273
Richard Beindorf <richard_beindorf@web.de>
2024-05-10 16:34:14 +0200

    components succesfully outsourced

--- components/Spotlight.js
+++ components/Spotlight.js
@@ -1,36 +1,14 @@
-import styled from "styled-components";
-import Image from "next/image";
-
-const StyledArticle = styled.article`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
-  border-width: 1px;
-  border-style: solid;
-  border-radius: 10px;
-  border-color: black;
-  margin: 0px 20px 0 20px;
-`;
-
-const StyledImage = styled(Image)`
-  border-radius: 10px;
-  object-fit: cover;
-`;
-
-const H2 = styled.h2`
-  text-align: center;
-`;
+import { Article, SpotlightImage, Title } from "./styledComponents/Spotlight.styles";
 
 export default function Spotlight({ title, area, image }) {
   return (
     <>
-      <StyledArticle>
-        <H2>Random Activity</H2>
+      <Article>
+        <Title>Random Activity</Title>
         <p>{title}</p>
-        <StyledImage src={image} width={281.25} height={210.75} alt="" />
+        <SpotlightImage src={image} width={281.25} height={210.75} alt="" />
         <p>{area}</p>
-      </StyledArticle>
+      </Article>
     </>
   );
 }
--- components/WeatherDisplay.js
+++ components/WeatherDisplay.js
@@ -1,49 +1,5 @@
-import styled from "styled-components";
 import useSWR from "swr";
-
-const WeatherSection = styled.section`
-  margin: 40px 20px 50px;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-`;
-
-const Area = styled.p`
-  font-size: 18px;
-  font-weight: 700;
-  width: 40%;
-  text-align: center;
-`;
-
-const Temperature = styled.p`
-  font-size: 26px;
-  font-weight: 700;
-  width: 40%;
-  text-align: center;
-`;
-
-const WeatherIcon = styled.img`
-  width: 90px;
-`;
-
-const Error = styled.p`
-  margin: 40px 20px 50px;
-  background-color: #ff6666;
-  text-align: center;
-  font-size: 18px;
-  font-weight: 600;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-`;
-
-const StyledSpan = styled.span`
-  font-size: 25px;
-  font-weight: bold;
-  color: #ffffff;
-`;
+import { WeatherSection, Area, Temperature, WeatherIcon, Error, Span } from "./styledComponents/WeatherDisplay.styles";
 
 const API = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 
@@ -70,7 +26,7 @@ export default function WeatherDisplay({ area }) {
       ) : (
         <Error>
           No weather data available for: <br />
-          <StyledSpan>{area}</StyledSpan>
+          <Span>{area}</Span>
         </Error>
       )}
     </>
