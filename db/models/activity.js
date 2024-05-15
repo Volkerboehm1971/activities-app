a66660ca8bed04f214c888057648453543fefaf9
Dominik Speidel <dominik-speidel@posteo.de>
2024-05-15 12:31:46 +0200

    unneccessary comments deleted

--- db/models/activity.js
+++ db/models/activity.js
@@ -14,18 +14,6 @@ const activitySchema = new Schema({
   lng: Number,
 });
 
-// const activitySchema = new Schema({
-//   title: { type: String, required: true },
-//   category: { type: String, required: true },
-//   categoryFilter: { type: String, required: false },
-//   area: { type: String, required: true },
-//   country: { type: String, required: true },
-//   description: { type: String, required: true },
-//   image: { type: String, required: false },
-//   lat: { type: Number, required: true },
-//   lng: { type: Number, required: true },
-// });
-
 const Activity =
   mongoose.models.Activity || mongoose.model("Activity", activitySchema);
 
