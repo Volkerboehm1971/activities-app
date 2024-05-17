d8ec7962941e8676b5da90aa9e1f94c1440e0c09
Dominik Speidel <dominik-speidel@posteo.de>
2024-05-17 11:27:09 +0200

    Bug on DetailPage fixed

--- components/DetailMap.js
+++ components/DetailMap.js
@@ -2,24 +2,28 @@ import { TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { StyledMapContainer } from "./styledComponents/DetailMap.styles";
 import { Icon } from "leaflet";
+import { useState, useEffect } from "react";
 
-
-export default function DetailMap({ longitude, latitude }) {
-
+export default function DetailMap({ latitude, longitude }) {
   const customIcon = new Icon({
     iconUrl:
       "https://icons.iconarchive.com/icons/steve/zondicons/512/Location-icon.png",
     iconSize: [30, 30],
   });
 
-  const geocode = [latitude, longitude];
   return (
-    <StyledMapContainer center={geocode} zoom={10} zoomControl={false}>
+    <StyledMapContainer
+      //The Key serves as Rerenderer of this component. Without him, the updated Geodata(after saving it in the FormEdit) would not be dislayed for the first run, only after a rerendering
+      key={`${latitude}-${longitude}`}
+      center={[latitude, longitude]}
+      zoom={10}
+      zoomControl={false}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={geocode} icon={customIcon}></Marker>
+      <Marker position={[latitude, longitude]} icon={customIcon}></Marker>
     </StyledMapContainer>
   );
 }
