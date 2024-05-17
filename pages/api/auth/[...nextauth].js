7e56604ce7a93fddffa776e78bbf76344638141c
Richard Beindorf <richard_beindorf@web.de>
2024-05-17 16:22:59 +0200

    added hard coded credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,47 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
-    GoogleProvider({
+    process.env.VERCEL_ENV !== "preview" && GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code"
-        }
-      }
     }),
-    GithubProvider({
+    process.env.VERCEL_ENV !== "preview" && GithubProvider({
         clientId: process.env.AUTH_GITHUB_ID,
         clientSecret: process.env.AUTH_GITHUB_SECRET,
       }),
-  ],
-}
+      CredentialsProvider({
+        name: "credentials",
+        credentials: {
+          username: { label: "Username", type: "text", placeholder: "username" },
+          password: { label: "Password", type: "password" },
+        },
+
+        async authorize(credentials) {
+          if (
+            credentials.username === "fisch" &&
+            credentials.password === "fisch"
+          ) {
+            return {
+              name: "Max",
+              email: "test@example.com",
+            };
+          } if (
+            credentials.username === "person" &&
+            credentials.password === "person"
+          ) {
+            return {
+              name: "User",
+              email: "user@example.com",
+            };
+          }
+          return null;
+        },
+      }),
+    ],
+  };
 
 export default NextAuth(authOptions)
