06674b9ce43ddf6c97bc2bb7c9bf8442d92e363b
Dominik Speidel <dominik-speidel@posteo.de>
2024-05-19 13:51:13 +0200

    changed table logic

--- components/WeatherForecastDetailModal.js
+++ components/WeatherForecastDetailModal.js
@@ -7,9 +7,16 @@ const WeatherForecastField = styled.section`
   margin: 12px;
 `;
 
-const StyledHeadline = styled.tr`
-  margin: 0;
-  padding: 0;
+const StyledWeekendDays = styled.p`
+  font-size: 15px;
+  font-weight: 600;
+`;
+
+const WeekendDiv = styled.div`
+  height: 175px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Column = styled.div`
@@ -20,7 +27,7 @@ const Column = styled.div`
 `;
 
 const StyledImage = styled.img`
-  width: 40px;
+  width: 55px;
 `;
 
 const Table = styled.table`
@@ -55,41 +62,106 @@ export default function WeatherForecastModal({
   filteredWeatherEvening,
   onClickButton,
 }) {
+  //   return (
+  //     <WeatherForecastField>
+  //       <Table>
+  //         <tbody>
+  //           <StyledHeadline>
+  //             <StyledTd></StyledTd>
+  //             {filteredWeatherMorning.slice(0, 3).map((weather, index) => (
+  //               <Td key={index}>
+  //                 <WeekdayFromDateString dateString={weather.dt_txt} />
+  //               </Td>
+  //             ))}
+  //           </StyledHeadline>
+  //           <tr>
+  //             <StyledTd>6am</StyledTd>
+  //             {filteredWeatherMorning.slice(0, 3).map((weather, index) => (
+  //               <Td key={index}>
+  //                 <Column>
+  //                   <StyledImage
+  //                     alt="WeatherMorning"
+  //                     src={`weatherIcons/${weather.weather[0].icon}.png`}
+  //                   />
+  //                   <StyledP>
+  //                     <strong>{Math.round(weather.main.temp)}°C</strong>
+  //                   </StyledP>
+  //                   <StyledP>Wind</StyledP>
+  //                   <StyledP>{weather.wind.speed} m/s</StyledP>
+  //                 </Column>
+  //               </Td>
+  //             ))}
+  //           </tr>
+  //           <tr>
+  //             <StyledTd>12am</StyledTd>
+  //             {filteredWeatherAfternoon.slice(0, 3).map((weather, index) => (
+  //               <Td key={index}>
+  //                 <Column>
+  //                   <StyledImage
+  //                     alt="WeatherAfternoon"
+  //                     src={`weatherIcons/${weather.weather[0].icon}.png`}
+  //                   />
+  //                   <StyledP>
+  //                     <strong>{Math.round(weather.main.temp)}°C</strong>
+  //                   </StyledP>
+
+  //                   <ArrowIcon deg={weather.wind.deg} />
+
+  //                   <StyledP>{weather.wind.speed} m/s</StyledP>
+  //                 </Column>
+  //               </Td>
+  //             ))}
+  //           </tr>
+  //           <tr>
+  //             <StyledTd>18pm</StyledTd>
+  //             {filteredWeatherEvening.slice(0, 3).map((weather, index) => (
+  //               <Td key={index}>
+  //                 <Column>
+  //                   <StyledImage
+  //                     alt="WeatherEvening"
+  //                     src={`weatherIcons/${weather.weather[0].icon}.png`}
+  //                   />
+  //                   <StyledP>
+  //                     <strong>{Math.round(weather.main.temp)}°C</strong>
+  //                   </StyledP>
+
+  //                   <ArrowIcon deg={weather.wind.deg} />
+
+  //                   <StyledP>{weather.wind.speed} m/s</StyledP>
+  //                 </Column>
+  //               </Td>
+  //             ))}
+  //           </tr>
+  //         </tbody>
+  //       </Table>
+  //     </WeatherForecastField>
+  //   );
+  // }
+
   return (
     <WeatherForecastField>
       <Table>
         <tbody>
-          <StyledHeadline>
-            <StyledTd></StyledTd>
+          <td>
+            <WeekendDiv>
+              <StyledWeekendDays>Monday</StyledWeekendDays>
+            </WeekendDiv>
+
+            <WeekendDiv>
+              <StyledWeekendDays>Monday</StyledWeekendDays>
+            </WeekendDiv>
+
+            <WeekendDiv>
+              <StyledWeekendDays>Monday</StyledWeekendDays>
+            </WeekendDiv>
+          </td>
+          <td>
+            <StyledP>
+              <strong>6am</strong>
+            </StyledP>
             {filteredWeatherMorning.slice(0, 3).map((weather, index) => (
-              <Td key={index}>
-                <WeekdayFromDateString dateString={weather.dt_txt} />
-              </Td>
-            ))}
-          </StyledHeadline>
-          <tr>
-            <StyledTd>6am</StyledTd>
-            {filteredWeatherMorning.slice(0, 3).map((weather, index) => (
-              <Td key={index}>
-                <Column>
-                  <StyledImage
-                    alt="WeatherMorning"
-                    src={`weatherIcons/${weather.weather[0].icon}.png`}
-                  />
-                  <StyledP>
-                    <strong>{Math.round(weather.main.temp)}°C</strong>
-                  </StyledP>
-                  <StyledP>Wind</StyledP>
-                  <StyledP>{weather.wind.speed} m/s</StyledP>
-                </Column>
-              </Td>
-            ))}
-          </tr>
-          <tr>
-            <StyledTd>12am</StyledTd>
-            {filteredWeatherAfternoon.slice(0, 3).map((weather, index) => (
-              <Td key={index}>
-                <Column>
+              <>
+                <Column key={index}>
                   <StyledImage
                     alt="WeatherAfternoon"
                     src={`weatherIcons/${weather.weather[0].icon}.png`}
@@ -97,34 +169,55 @@ export default function WeatherForecastModal({
                   <StyledP>
                     <strong>{Math.round(weather.main.temp)}°C</strong>
                   </StyledP>
-
                   <ArrowIcon deg={weather.wind.deg} />
-
                   <StyledP>{weather.wind.speed} m/s</StyledP>
                 </Column>
-              </Td>
+                <hr></hr>
+              </>
             ))}
-          </tr>
-          <tr>
-            <StyledTd>18pm</StyledTd>
-            {filteredWeatherEvening.slice(0, 3).map((weather, index) => (
-              <Td key={index}>
-                <Column>
+          </td>
+          <td>
+            <StyledP>
+              <strong>12am</strong>
+            </StyledP>
+            {filteredWeatherAfternoon.slice(0, 3).map((weather, index) => (
+              <>
+                <Column key={index}>
                   <StyledImage
-                    alt="WeatherEvening"
+                    alt="WeatherAfternoon"
                     src={`weatherIcons/${weather.weather[0].icon}.png`}
                   />
                   <StyledP>
                     <strong>{Math.round(weather.main.temp)}°C</strong>
                   </StyledP>
-
                   <ArrowIcon deg={weather.wind.deg} />
-
                   <StyledP>{weather.wind.speed} m/s</StyledP>
                 </Column>
-              </Td>
+                <hr></hr>
+              </>
             ))}
-          </tr>
+          </td>
+          <td>
+            <StyledP>
+              <strong>18pm</strong>
+            </StyledP>
+            {filteredWeatherEvening.slice(0, 3).map((weather, index) => (
+              <>
+                <Column key={index}>
+                  <StyledImage
+                    alt="WeatherAfternoon"
+                    src={`weatherIcons/${weather.weather[0].icon}.png`}
+                  />
+                  <StyledP>
+                    <strong>{Math.round(weather.main.temp)}°C</strong>
+                  </StyledP>
+                  <ArrowIcon deg={weather.wind.deg} />
+                  <StyledP>{weather.wind.speed} m/s</StyledP>
+                </Column>
+                <hr></hr>
+              </>
+            ))}
+          </td>
         </tbody>
       </Table>
     </WeatherForecastField>
