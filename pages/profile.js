845c7fbd5045292faf74d1dd563e23495be61cda
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-05-20 12:35:09 +0200

    Integrating BookmarkActivity-Functionality on Profile page as well

--- pages/profile.js
+++ pages/profile.js
@@ -7,9 +7,12 @@ import {
   LinkDetailsPage,
   ErrorMessage,
 } from "@/components/styledComponents/profile.styled";
+import Bookmark from "@/components/Bookmark";
+import { BookmarkContainer } from "@/components/styledComponents/activityList.styles";
 import ActivityCard from "@/components/ActivityCard";
 
 export default function ProfilePage({
+  onHandleBookmark,
   activities = [],
   bookmarkedActivities = [],
 }) {
@@ -28,6 +31,13 @@ export default function ProfilePage({
         <Ul>
           {bookmarkedActivitiesDetails.map((activity) => (
             <Li key={activity.id}>
+              <BookmarkContainer>
+                <Bookmark
+                  onHandleBookmark={onHandleBookmark}
+                  bookmarkedActivities={bookmarkedActivities}
+                  activity={activity}
+                />
+              </BookmarkContainer>
               <LinkDetailsPage href={`/${activity.id}`}>
                 <ActivityCard
                   id={activity.id}
