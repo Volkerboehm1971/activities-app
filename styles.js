fb0c14730c6f69d03effc0a2ca75fa9a89a54ecf
Richard Beindorf <richard_beindorf@web.de>
2024-05-23 11:46:37 +0200

    added new map icon

--- styles.js
+++ styles.js
@@ -10,8 +10,6 @@ export default createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    /* font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-family: "Roboto Flex"; */
     font-size: 10px;
   }
 `;
