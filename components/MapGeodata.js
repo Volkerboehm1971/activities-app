7494edfceda73abcf54ef82a62aaf004fa96f9fb
Dominik Speidel <dominik-speidel@posteo.de>
2024-05-24 14:50:23 +0200

    Click Overlay Close Modal MapGeodata.js

--- components/MapGeodata.js
+++ components/MapGeodata.js
@@ -27,7 +27,7 @@ export default function MapGeodata({
   defaultLongitude,
 }) {
   return (
-    <Overlay>
+    <Overlay onClick={onClickClose}>
       <Container>
         <InformationDisplay>
           <SelectedLatLng>
