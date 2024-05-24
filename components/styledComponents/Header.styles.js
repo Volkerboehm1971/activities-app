15b08422b693b0274b2f358b6ef065766d76b0c2
Dominik Speidel <dominik-speidel@posteo.de>
2024-05-24 10:42:12 +0200

    changed Headline size

--- components/styledComponents/Header.styles.js
+++ components/styledComponents/Header.styles.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const StyledHeaderContainer = styled.section`
-display:flex;
-flex-direction: column;
-align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const Headline = styled.h1`
+  margin: 0;
+  font-size: 18px;
+  font-weight: 700;
 `;
