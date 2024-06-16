301a51f99fb82d65d03f829872a81abc6f934cfe
Richard Beindorf <richard_beindorf@web.de>
2024-06-16 15:15:39 +0200

    fixed error by chatgpt

--- components/FormEdit.js
+++ components/FormEdit.js
@@ -119,206 +119,175 @@ export default function FormEdit({ id, activityToEdit }) {
   };
 
   return (
-      <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit}>
+      <Section>
+        <label htmlFor="title">Aktivitätsname</label>
+        <Input
+          id="title"
+          name="title"
+          type="text"
+          minLength="1"
+          maxLength="150"
+          pattern="^(?!.*\s{2,}).+$"
+          defaultValue={activityToEdit?.title}
+          required
+        />
+      </Section>
+      <Section>
+        <label htmlFor="category">Kategorie der Aktivität</label>
+        <Select
+          id="category"
+          name="category"
+          defaultValue={`${activityToEdit?.categoryFilter}-${activityToEdit?.category}`}
+        >
+          <option value="">
+            --Kategorie hier ändern--
+          </option>
+          <option value="Water-Surfsport">Surfsport</option>
+          <option value="Water-Sailing">Segeln</option>
+          <option value="Water-Swimming">Schwimmen</option>
+          <option value="Water-Paddeling">Paddeln</option>
+          <option value="Water-Diving">Tauchen</option>
+          <option value="Winter-Skiing">Skifahren</option>
+          <option value="Winter-Tobogganing">Rodeln</option>
+          <option value="Winter-Ice skating">Schlittschuhlaufen</option>
+          <option value="Winter-Snowboarding">Snowboarden</option>
+          <option value="Biking-Mountainbiking">Mountainbiking</option>
+          <option value="Biking-Cycling">Radfahren</option>
+          <option value="Hiking-Strolling">Spazieren</option>
+          <option value="Hiking-(Nordic) Walking">(Nordic) Walking</option>
+          <option value="Hiking-Jogging">Joggen</option>
+          <option value="Hiking-Hiking">Wandern</option>
+          <option value="Extrem-Paragliding">Gleitschirmfliegen</option>
+          <option value="Extrem-Bungeejumping">Bungeejumping</option>
+          <option value="Extrem-Motorcross">Motocross</option>
+          <option value="Extrem-Rafting">Rafting</option>
+          <option value="Indoor-Curling">Curling</option>
+          <option value="Indoor-Bouldern">Bouldern</option>
+          <option value="Indoor-Ice Hockey">Eishockey</option>
+          <option value="Others-Others">Sonstiges</option>
+        </Select>
+      </Section>
+      <TinyInputsWrapper>
         <Section>
-          <label htmlFor="title">Activity Name</label>
-          <Input
-            id="title"
-            name="title"
+          <label htmlFor="area">Bereich</label>
+          <TinyInput
+            id="area"
+            name="area"
             type="text"
-            minLength="1"
-            maxLength="150"
             pattern="^(?!.*\s{2,}).+$"
-            defaultValue={activityToEdit?.title}
+            defaultValue={activityToEdit?.area}
             required
           />
         </Section>
-
         <Section>
-          <label htmlFor="category">Category of Activity</label>
-          <Select
-            id="category"
-            name="category"
-            defaultValue={`${activityToEdit?.categoryFilter}-${activityToEdit?.category}`}
-          >
-            <option
-              defaultValue={`${activityToEdit?.categoryFilter}-${activityToEdit?.category}`}
+          <label htmlFor="country">Land</label>
+          <TinyInput
+            id="country"
+            name="country"
+            type="text"
+            pattern="^(?!.*\s{2,}).+$"
+            defaultValue={activityToEdit?.country}
+            required
+          />
+        </Section>
+        <Section>
+          <label htmlFor="longitude">Längengrad</label>
+          <TinyDiv>
+            {longitude !== null ? longitude : activityToEdit?.lng}
+          </TinyDiv>
+        </Section>
+        <Section>
+          <label htmlFor="latitude">Breitengrad</label>
+          <TinyDiv>
+            {latitude !== null ? latitude : activityToEdit?.lat}
+          </TinyDiv>
+        </Section>
+      </TinyInputsWrapper>
+      <ModalContainer>
+        <div onClick={() => setShowModal(!showModal)}>Geodaten auswählen</div>
+      </ModalContainer>
+      {showModal && (
+        <MapGeodata
+          onClickClose={() => setShowModal(!showModal)}
+          onHandleClick={handleClick}
+          latitude={latitude}
+          longitude={longitude}
+          clickedPosition={clickedPosition}
+          defaultLatitude={activityToEdit?.lat}
+          defaultLongitude={activityToEdit?.lng}
+        ></MapGeodata>
+      )}
+      <Section>
+        <label htmlFor="description">Beschreibung</label>
+        <Textarea
+          id="description"
+          rows="7"
+          name="description"
+          type="text"
+          pattern="^(?!.*\s{2,}).+$"
+          defaultValue={activityToEdit?.description}
+          required
+        />
+      </Section>
+      <WrapperSearchBar>
+        <label htmlFor="image">Aktivitätsbild suchen</label>
+        <InputSearchField
+          id="image"
+          name="image"
+          type="text"
+          value={searchTerm}
+          onChange={handleKeyPress}
+        />
+      </WrapperSearchBar>
+      {typingInSearchbar && (
+        <ContainerSwitchesAndPicture>
+          <ButtonWrapper>
+            <MinusButton
+              onClick={() => {
+                if (increment >= 1) {
+                  setIncrement((prevCount) => prevCount - 1);
+                }
+              }}
             >
-              --change category here--
-            </option>
-            <option value="Water-Surfsport">Surfsport</option>
-            <option value="Water-Sailing">Sailing</option>
-            <option value="Water-Swimming">Swimming</option>
-            <option value="Water-Paddeling">Paddeling</option>
-            <option value="Water-Diving">Diving</option>
-
-            <option value="Winter-Skiing">Skiing</option>
-            <option value="Winter-Tobogganing">Tobogganing</option>
-            <option value="Winter-Ice skating">Ice skating</option>
-            <option value="Winter-Snowboarding">Snowboarding</option>
-
-            <option value="Biking-Mountainbiking">Mountainbiking</option>
-            <option value="Biking-Cycling">Cycling</option>
-
-            <option value="Hiking-Strolling">Strolling</option>
-            <option value="Hiking-(Nordic) Walking">(Nordic) Walking</option>
-            <option value="Hiking-Jogging">Jogging</option>
-            <option value="Hiking-Hiking">Hiking</option>
-
-            <option value="Extrem-Paragliding">Paragliding</option>
-            <option value="Extrem-Bungeejumping">Bungeejumping</option>
-            <option value="Extrem-Motorcross">Motorcross</option>
-            <option value="Extrem-Rafting">Rafting</option>
-
-            <option value="Indoor-Curling">Curling</option>
-            <option value="Indoor-Bouldern">Bouldern</option>
-            <option value="Indoor-Ice Hockey">Ice Hockey</option>
-
-            <option value="Others-Others">Others</option>
-          </Select>
-        </Section>
-
-        <TinyInputsWrapper>
-          <Section>
-            <label htmlFor="area">Area</label>
-            <TinyInput
-              id="area"
-              name="area"
-              type="text"
-              pattern="^(?!.*\s{2,}).+$"
-              defaultValue={activityToEdit?.area}
-              required
-            />
-          </Section>
-          <Section>
-            <label htmlFor="country">Country</label>
-            <TinyInput
-              id="country"
-              name="country"
-              type="text"
-              pattern="^(?!.*\s{2,}).+$"
-              defaultValue={activityToEdit?.country}
-              required
-            />
-          </Section>
-
-          <Section>
-            <label htmlFor="area">Longitude</label>
-            <TinyDiv>
-              {longitude !== null ? longitude : activityToEdit?.lng}
-            </TinyDiv>
-          </Section>
-          <Section>
-            <label htmlFor="area">Latitude</label>
-            <TinyDiv>
-              {latitude !== null ? latitude : activityToEdit?.lat}
-            </TinyDiv>
-          </Section>
-        </TinyInputsWrapper>
-
-        <ModalContainer>
-          <div onClick={() => setShowModal(!showModal)}>Select Geodata</div>
-        </ModalContainer>
-
-        {showModal && (
-          <MapGeodata
-            onClickClose={() => setShowModal(!showModal)}
-            onHandleClick={handleClick}
-            latitude={latitude}
-            longitude={longitude}
-            clickedPosition={clickedPosition}
-            defaultLatitude={activityToEdit?.lat}
-            defaultLongitude={activityToEdit?.lng}
-          ></MapGeodata>
-        )}
-
-        <Section>
-          <label htmlFor="description">Description</label>
-          <Textarea
-            id="description"
-            rows="7"
-            name="description"
-            type="text"
-            pattern="^(?!.*\s{2,}).+$"
-            defaultValue={activityToEdit?.description}
-            required
-          />
-        </Section>
-
-        <WrapperSearchBar>
-          <label htmlFor="image">Search Activity Image</label>
-          <InputSearchField
-            id="image"
-            name="image"
-            type="text"
-            value={searchTerm}
-            onChange={handleKeyPress}
-          />
-        </WrapperSearchBar>
-        {typingInSearchbar ? (
-          <ContainerSwitchesAndPicture>
-            <ButtonWrapper>
-              <MinusButton
-                onClick={() => {
-                  if (increment >= 1) {
-                    setIncrement((prevCount) => prevCount - 1);
-                  }
-                }}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 700 960"
+                width="24px"
+                fill="#000000"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 700 960"
-                  width="24px"
-                  fill="#000000"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24px"
-                    viewBox="0 -960 960 960"
-                    width="24px"
-                    fill="#000000"
-                  >
-                    <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
-                  </svg>
-                </MinusButton>
-                <p>
-                  {increment + 1}/
-                  {typingInSearchbar & searchingForImage
-                    ? imageSearch.hits.length
-                    : "1"}
-                </p>
-                <PlusButton
-                  onClick={() => {
-                    if (defaultImage){setIncrement(1)} else if (increment < imageSearch.hits.length - 1) {
-                      setIncrement((prevCount) => prevCount + 1);
-                    }
-                  }}
-                >
-                  <path d="M579-480 285-774q-15-15-14.5-35.5T286-845q15-15 35.5-15t35.5 15l307 308q12 12 18 27t6 30q0 15-6 30t-18 27L356-115q-15 15-35 14.5T286-116q-15-15-15-35.5t15-35.5l293-293Z" />
-                </svg>
-              </PlusButton>
-            </ButtonWrapper>
-            {isLoading ? (
-              <ImageSkeleton />
-            ) : (
-              <ImageContainer>
-                <SearchImage
-                  src={defaultOrSearchedImage}
-                  fill
-                  alt="Pixabay Image"
-                />
-              </ImageContainer>
-            )}
-          </WrapperSearchAndSwitch>
+                <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
+              </svg>
+            </MinusButton>
+            <p>
+              {increment + 1}/
+              {typingInSearchbar && searchingForImage
+                ? imageSearch.hits.length
+                : "1"}
+            </p>
+            <PlusButton
+              onClick={() => {
+                if (defaultImage) {
+                  setIncrement(1);
+                } else if (increment < imageSearch.hits.length - 1) {
+                  setIncrement((prevCount) => prevCount + 1);
+                }
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#000000"
+              >
+                <path d="M579-480 285-774q-15-15-14.5-35.5T286-845q15-15 35.5-15t35.5 15l307 308q12 12 18 27t6 30q0 15-6 30t-18 27L356-115q-15 15-35 14.5T286-116q-15-15-15-35.5t15-35.5l293-293Z" />
+              </svg>
+            </PlusButton>
+          </ButtonWrapper>
           {isLoading ? (
-          <ImageContainer>              
-          <SearchImage
-            src={defaultOrSearchedImage}
-            fill
-            alt="Pixabay Image"
-          />
-          </ImageContainer>
+            <ImageSkeleton />
           ) : (
             <ImageContainer>
               <SearchImage
@@ -328,12 +297,12 @@ export default function FormEdit({ id, activityToEdit }) {
               />
             </ImageContainer>
           )}
-        </Section>
-
-        <ButtonContainer>
-          <LinkCancel href={`/${id}`}>Cancel</LinkCancel>
-          <ButtonSave type="submit">Save</ButtonSave>
-        </ButtonContainer>
-      </Form>
+        </ContainerSwitchesAndPicture>
+      )}
+      <ButtonContainer>
+        <LinkCancel href={`/${id}`}>Abbrechen</LinkCancel>
+        <ButtonSave type="submit">Speichern</ButtonSave>
+      </ButtonContainer>
+    </Form>
   );
 }
